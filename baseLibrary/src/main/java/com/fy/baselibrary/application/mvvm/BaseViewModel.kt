a766702e61ef0d44c50a40b3526c3b995960b55d
fangs <fy310518@163.com>
2021-09-16 10:32:10 +0800

    mvvm 初识

--- baseLibrary/src/main/java/com/fy/baselibrary/application/mvvm/BaseViewModel.kt
+++ baseLibrary/src/main/java/com/fy/baselibrary/application/mvvm/BaseViewModel.kt
@@ -2,13 +2,14 @@ package com.fy.baselibrary.application.mvvm
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.fy.baselibrary.application.ioc.ConfigUtils
 import com.fy.baselibrary.utils.notify.L
 
 /**
  * description 定义 MVVM ViewModel 基类
  * Created by fangs on 2021/9/10 9:32.
  */
-class BaseViewModel(application: Application) : AndroidViewModel(application) {
+class BaseViewModel: AndroidViewModel(ConfigUtils.getAppCtx() as Application) {
 
     override fun onCleared() {
         super.onCleared()
