8442c2e1ef04e280ec8a0ad72543dd220ed58277
fangs <fy310518@163.com>
2024-09-26 09:09:31 +0800

    Android14 注册广播报错解决

--- baseLibrary/src/main/java/com/fy/baselibrary/utils/ResultsUtils.kt
+++ baseLibrary/src/main/java/com/fy/baselibrary/utils/ResultsUtils.kt
@@ -1,13 +1,18 @@
 package com.fy.baselibrary.utils
 
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 /**
@@ -27,10 +32,13 @@ fun Fragment.registerActResult(callback: ActivityResultCallback<ActivityResult>)
         callback.onActivityResult(it)
     }
 
-fun Fragment.registerPermissionResult(callback: ActivityResultCallback<Map<String, @JvmSuppressWildcards Boolean>>) =
-    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-        callback.onActivityResult(it)
-    }
+//fun Fragment.registerPermissionResult(callback: ActivityResultCallback<Map<String, @JvmSuppressWildcards Boolean>>) =
+//    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { premissions->
+////        val granted = premissions.entries.all {
+////            it.value == true
+////        }
+//        callback.onActivityResult(premissions)
+//    }
 
 fun Intent.launch(launcher: ActivityResultLauncher<Intent>) {
     launcher.launch(this)
@@ -41,4 +49,16 @@ fun Bundle.result(activity: Activity) {
     intent.putExtras(this)
     activity.setResult(Activity.RESULT_OK, intent)
     activity.finish()
+}
+
+/**
+ * Android14 注册广播报错解决
+ * [RECEIVER_EXPORTED 表示可以接收应用外部广播，ContextRECEIVER_NOT_EXPORTED 应用内部广播]
+ */
+fun Context.registerReceiver(receiver: BroadcastReceiver, filter: IntentFilter, flags: Int = Context.RECEIVER_EXPORTED){
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        registerReceiver(receiver, filter, flags)
+    } else {
+        registerReceiver(receiver, filter)
+    }
 }
